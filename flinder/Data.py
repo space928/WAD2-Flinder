c121d4827148993ab6e4077827b86572d2da5a1e
CoderiGenius <29704758+CoderiGenius@users.noreply.github.com>
2022-03-14 21:33:45 +0000

    Add database population script

---
+++ flinder/Data.py
@@ -0,0 +1,114 @@
+def Get_data():
+    users = [{
+	"user": {
+		"userName": "Zhang",
+		"email": "Zhang@gmail.com"
+	},
+	"user_profile": {
+		"name": "Zhang",
+		"lastName": "Zhang",
+		"emailAddress": "Zhang@gmail.com",
+		"dateOfBirth": "2000-11-11",
+		"flatSearcher": True,
+		"gender": "F",
+		"yearOfStudy": "2"
+	},
+	"picture": {
+		"picture": "https://robohash.org/quialaudantiumqui.png?size=50x50&set=set1",
+		"description": "This is a description"
+	},
+	"interestsAndPriorities": {
+		"pets": True,
+		"food": True,
+		"sports": True,
+		"music": True,
+		"partying": True,
+		"flatCleanliness": True,
+		"strictQuietHours": True,
+		"drinking": True
+	},
+	"preferences": {
+		"mixedGender": True,
+		"mixedAge": True,
+		"mixedYearOfStudy": True,
+		"maximumDistance": "10"
+	}
+},
+
+
+
+		{
+			"user": {
+				"userName": "XiaoLong",
+				"email": "xiaolong@gmail.com"
+			},
+			"user_profile": {
+				"name": "Mike Guan",
+				"lastName": "Mike",
+				"emailAddress": "xiaolong@gmail.com",
+				"dateOfBirth": "2000-12-11",
+				"flatSearcher": True,
+				"gender": "M",
+				"yearOfStudy": "1"
+			},
+			"picture": {
+				"picture": "https://robohash.org/namidfacilis.png?size=50x50&set=set1",
+				"description": "This is a description"
+			},
+			"interestsAndPriorities": {
+				"pets": True,
+				"food": True,
+				"sports": True,
+				"music": True,
+				"partying": True,
+				"flatCleanliness": True,
+				"strictQuietHours": True,
+				"drinking": True
+			},
+			"preferences": {
+				"mixedGender": True,
+				"mixedAge": True,
+				"mixedYearOfStudy": True,
+				"maximumDistance": "10"
+			}
+		},
+
+
+
+		{
+			"user": {
+				"userName": "Peter",
+				"email": "Peter@gmail.com"
+			},
+			"user_profile": {
+				"name": "Peter",
+				"lastName": "Peter",
+				"emailAddress": "Peter@gmail.com",
+				"dateOfBirth": "1997-12-11",
+				"flatSearcher": True,
+				"gender": "M",
+				"yearOfStudy": "4"
+			},
+			"picture": {
+				"picture": "https://robohash.org/eiusveritatisiusto.png?size=50x50&set=set1",
+				"description": "This is a description"
+			},
+			"interestsAndPriorities": {
+				"pets": True,
+				"food": True,
+				"sports": True,
+				"music": True,
+				"partying": True,
+				"flatCleanliness": True,
+				"strictQuietHours": True,
+				"drinking": True
+			},
+			"preferences": {
+				"mixedGender": True,
+				"mixedAge": True,
+				"mixedYearOfStudy": True,
+				"maximumDistance": "10"
+			}
+		}
+	]
+    return users
