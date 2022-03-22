def386ab3179f785e42bc25b77ba2d66b41dbe49
CoderiGenius <29704758+CoderiGenius@users.noreply.github.com>
2022-03-22 22:03:57 +0000

    FLN - 32 - Refactoring

--- flinder/flinderserver/api.py
+++ flinder/flinderserver/api.py
@@ -3,15 +3,20 @@ from django.http import JsonResponse
 from flinderserver.models import Swipe
 
 
+# api/get_match?id=1
+# result example:
+# {
+# 	"status": "0",
+# 	"msg": "",
+# 	"data": []
+# }
+
 def get_match(request):
     id = request.GET.get('id')
     result = Swipe.objects.filter(swiper_id=id)
     result_list = []
     for title in result:
-        member = {}
-        member["swiped_id"] = title.swiped_id
-        member["swiper_id"] = title.swiper_id
-        member["swipeRight"] = title.swipeRight
+        member = {"swiped_id": title.swiped_id, "swiper_id": title.swiper_id, "swipeRight": title.swipeRight}
         result_list.append(member)
 
     return JsonResponse({"status": "0", "msg": "", "data": result_list})
