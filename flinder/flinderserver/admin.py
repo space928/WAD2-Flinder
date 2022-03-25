6dd38ed9c8ad456975f467d8c3718e1f6f5b2810
Thomas Mathieson <thomas.mathieson02@gmail.com>
2022-03-25 23:29:23 +0000

    Revert "Forms fix"

--- flinder/flinderserver/admin.py
+++ flinder/flinderserver/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from flinderserver.models import UserProfile, Pictures, Swipe
+from flinderserver.models import UserProfile, Pictures, InterestsAndPriorities, Swipe
 
 admin.site.register(UserProfile)
 admin.site.register(Pictures)
+admin.site.register(InterestsAndPriorities)
 admin.site.register(Swipe)
--- flinder/flinderserver/models.py
+++ flinder/flinderserver/models.py
@@ -1,26 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
-from multiselectfield import MultiSelectField
-import uuid
 
 
-class UserProfile(models.Model):
-    YEAR_OF_STUDY_CHOICES = (
-        ('1', '1st Year'),
-        ('2', '2nd Year'),
-        ('3', '3rd Year'),
-        ('4', '4th Year'),
-        ('5', '5th Year'),
-        ('PGT', 'Postgraduate'),
-        ('MIX', 'Mixed'),  # for flat providers
-    )
-    GENDER_CHOICES = (
-        ('M', 'Male'),
-        ('F', 'Female'),
-        ('O', 'Other'),
-        ('MIX', 'Mixed'),  # for flat providers
-        ('PNTS', 'Prefer not to say'),
-    )
+class InterestsAndPriorities(models.Model):
     INTERESTS_CHOICES = (
         ('pets', 'pets'),
         ('food', 'food'),
@@ -31,40 +13,60 @@ class UserProfile(models.Model):
         ('flatCleanliness', 'flatCleanliness'),
         ('strictQuietHours', 'strictQuietHours')
     )
-    BOOL_CHOICES = ((True, 'Mixed'), (False, 'Same'))
+    choice = models.CharField(max_length=20, unique=True)
 
+    class Meta:
+        verbose_name_plural = 'Interests and Priorities'
+
+    def __str__(self):
+        return self.choice
+
+
+class UserProfile(models.Model):
     # Fields shared between flat seekers and flat providers are optional
     username = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=128)
-    gender = models.CharField(max_length=4, choices=GENDER_CHOICES)
     yearOfBirth = models.IntegerField()
-    yearOfStudy = models.CharField(max_length=2, choices=YEAR_OF_STUDY_CHOICES)
-    university = models.CharField(max_length=30)
-    interests = MultiSelectField(choices=INTERESTS_CHOICES)
-    contactDetails = models.CharField(max_length=128)
-
     flatSearcher = models.BooleanField()
-
     addressLine1 = models.CharField(max_length=128, null=True, blank=True)
     addressLine2 = models.CharField(max_length=128, null=True, blank=True)
     postCode = models.CharField(max_length=7, null=True, blank=True)
     flatBedrooms = models.IntegerField(null=True, blank=True)
     freeBedrooms = models.IntegerField(null=True, blank=True)
-
+    university = models.CharField(max_length=30)
+    BOOL_CHOICES = ((True, 'Mixed'), (False, 'Same'))
     mixedGender = models.BooleanField(choices=BOOL_CHOICES)
     mixedYearOfStudy = models.BooleanField(choices=BOOL_CHOICES)
     mixedAge = models.BooleanField(choices=BOOL_CHOICES)
+    contactDetails = models.CharField(max_length=128)
+
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+        ('MIX', 'Mixed'),  # for flat providers
+        ('PNTS', 'Prefer not to say'),
+    )
+    gender = models.CharField(max_length=4, choices=GENDER_CHOICES)
+
+    YEAR_OF_STUDY_CHOICES = (
+        ('1', '1st Year'),
+        ('2', '2nd Year'),
+        ('3', '3rd Year'),
+        ('4', '4th Year'),
+        ('5', '5th Year'),
+        ('PGT', 'Postgraduate'),
+        ('MIX', 'Mixed'),  # for flat providers
+    )
+    yearOfStudy = models.CharField(max_length=2, choices=YEAR_OF_STUDY_CHOICES)
+    interests = models.ManyToManyField(InterestsAndPriorities)
 
     class Meta:
         verbose_name_plural = 'User Profiles'
-        indexes = [
-            models.Index(fields=['username', ]),
-            models.Index(fields=['uuid', ]),
-        ]
 
     def __str__(self):
-        return f"{self.name}, Username: {self.username.username}, room {('seeker' if self.flatSearcher else 'provider')}"
+        return self.name + ", Username: " + self.username + (
+            " room seeker" if self.flatSearcher else " room provider")
 
 
 class Pictures(models.Model):
@@ -77,7 +79,7 @@ class Pictures(models.Model):
         verbose_name_plural = 'Pictures'
 
     def __str__(self):
-        return f"PictureID: {self.pictureID}, posted by {self.poster}"
+        return "PictureID: " + self.pictureID + ", posted by " + self.poster
 
 
 class Swipe(models.Model):
@@ -90,4 +92,4 @@ class Swipe(models.Model):
         verbose_name_plural = 'Swipe Actions'
 
     def __str__(self):
-        return f"{self.swiper} swiped {self.swiped}. SwipeID: {self.swipeID}"
+        return self.swiper + " swiped " + self.swiped + ". SwipeID: " + self.swipeID
