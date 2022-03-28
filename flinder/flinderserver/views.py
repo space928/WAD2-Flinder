a57d65690b5dfab6f5e26e3b3d3b48ce0bc24d87
Gianmarco Cornacchia <2575307c@student.gla.ac.uk>
2022-03-28 16:58:32 +0100

    Edit editProfile to pre-populate view

--- flinder/flinderserver/views.py
+++ flinder/flinderserver/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.forms.models import model_to_dict
 
 from flinderserver.forms import RoomSeekerForm, RoomProviderForm, UserForm, UserProfileForm
 from flinderserver.models import UserProfile, Pictures, Swipe
@@ -73,7 +74,7 @@ def edit_profiles(request):
         else:
             print(user_form.errors)
     else:
-        user_form = UserProfileForm(request.POST, instance=user_profile)
+        user_form = UserProfileForm(request.POST, instance=user_profile, initial=model_to_dict(user_profile))
     return render(request, 'flinder/edit_profile.html', context={'user_form': user_form})
 
 
